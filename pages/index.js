89687aba1817dba9aaddecacc013b7a9a12b9e92
kmaleao <udijonson@gmail.com>
2022-06-03 18:35:14 -0300

    primeiro commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home(){
+    return(
+        
+        <div class="planeta planetaterra">
+            <span id="radio-on">radio-on</span>
+            <h2>Blueplanet</h2><a href='/playlist'>Curta outras playlists</a><br></br><iframe width="80%" height="200" src="https://www.youtube.com/embed/5X18D-EbjUc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="container1">
+                <div class="loader"><span></span></div>
+                <div class="terra"></div>
+            </div>
+        </div>
+    
+
+)}
---
+++ pages/playlist.js
@@ -0,0 +1,12 @@
+export default function playlist(){
+    return(
+        
+        <div class="planeta planetaterra">
+            
+            
+                <div class="loader"><span></span></div>
+                <div class="terra2"></div>
+            
+        </div>
+    
+ )}
