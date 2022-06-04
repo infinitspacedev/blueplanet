cd2ed611802964542135b2139dcec97f32616dbf
kmaleao <udijonson@gmail.com>
2022-06-04 12:31:45 -0300

    alok mp3

---
+++ pages/card01.js
@@ -0,0 +1,26 @@
+export default function card01(){
+    return(
+        
+       
+            
+                
+                <div class=' planeta planetaterra2'>
+                
+                    <div class='container2'>
+                        <div class="terra2"></div>
+                    </div>
+                   <h1>Radio-on</h1><br></br><button><a href='/playlist'>voltar</a></button>
+                   <div class='card01'>
+                       <h1>Alok</h1><br></br>
+                       <img class='card'src='/alok.jpg'></img><br></br><p> DJ  Alok   festival de musica vila mix em brasilia</p><br></br>
+                       <audio controls>
+                           <source src='/alok.mp3'></source>
+                       </audio>
+
+                   </div>
+                   
+                </div>
+            
+        
+    
+ )}
--- pages/playlist.js
+++ pages/playlist.js
@@ -1,12 +1,32 @@
 export default function playlist(){
     return(
         
-        <div class="planeta planetaterra">
+       
             
+                
+                <div class=' planeta planetaterra2'>
+                
+                    <div class='container2'>
+                        <div class="terra2"></div>
+                    </div>
+                   <h1>Radio-on</h1><br></br><button><a href='/'>voltar</a></button>
+                   <ul class='playlist'>
+                       <h3>melhores playlists </h3><br></br>
+                      
+                       <li><img class='img' src="/alok.jpg"></img><button><a href='/card01'>DJ Alok ouvir ou baixar</a></button></li>
+                       <li><img class='img' src="/alok.jpg"></img><button><a href='/'>xxxxxxxxxx</a></button></li>
+                       <li><img class='img' src="/alok.jpg"></img><button><a href='/'>xxxxxxxxxx</a></button></li>
+                       <li><img class='img' src="/alok.jpg"></img><button><a href='/'>xxxxxxxxxx</a></button></li>
+                       <li><img class='img' src="/alok.jpg"></img><button><a href='/'>xxxxxxxxxx</a></button></li>
+                       <li><img class='img' src="/alok.jpg"></img><button><a href='/'>xxxxxxxxxx</a></button></li>
+                       <li><img class='img' src="/alok.jpg"></img><button><a href='/'>xxxxxxxxxx</a></button></li>
+                       <li><img class='img' src="/alok.jpg"></img><button><a href='/'>xxxxxxxxxx</a></button></li>
+                       <li><img class='img' src="/alok.jpg"></img><button><a href='/'>xxxxxxxxxx</a></button></li>
+                       <li><img class='img' src="/alok.jpg"></img><button><a href='/'>xxxxxxxxxx</a></button></li>
+                       
+                   </ul>
+                </div>
             
-                <div class="loader"><span></span></div>
-                <div class="terra2"></div>
-            
-        </div>
+        
     
  )}
